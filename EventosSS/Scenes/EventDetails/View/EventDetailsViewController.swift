3809f46e14bdd7654ec3d22e2d9558cca8219e6a
Hiago <hiago.chagas.14@gmail.com>
2021-10-12 20:07:40 -0300

    Compartilhamento de um evento

--- EventosSS/Scenes/EventDetails/View/EventDetailsViewController.swift
+++ EventosSS/Scenes/EventDetails/View/EventDetailsViewController.swift
@@ -15,6 +15,7 @@ class EventDetailsViewController: UIViewController {
         self.view = contentView
         self.navigationItem.largeTitleDisplayMode = .never
         fullfilContentView()
+        shareButtonSetup()
     }
     
     private func fullfilContentView() {
@@ -27,4 +28,23 @@ class EventDetailsViewController: UIViewController {
         contentView.eventDescriptionLabel.text = event?.description
     }
     
+    private func shareButtonSetup() {
+        guard let image = UIImage(systemName: "square.and.arrow.up") else { return }
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(shareButtonTapped))
+    }
+    
+    @objc private func shareButtonTapped() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        let date = dateFormatter.string(from: event?.date ?? Date())
+        let message = """
+        Oi! Vi esse evento e lembrei de você! Gostaria de ir comigo? Detalhes:
+        Nome do evento: \(event?.title ?? "Sem título")
+        Data: \(date)
+        Preço do ingresso: R$ \(event?.price ?? 0.0)
+        E aí? Vamos?
+        """
+        let activityController = UIActivityViewController(activityItems: [message], applicationActivities: nil)
+        present(activityController, animated: true) { }
+    }
 }
