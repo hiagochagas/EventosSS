12a27575ec863cc7f57f4f7e2d480847f19e69a4
Hiago <hiago.chagas.14@gmail.com>
2021-10-12 18:50:15 -0300

    Inserção dos detalhes numa ScrollView

--- EventosSS/Scenes/EventDetails/View/EventDetailsView.swift
+++ EventosSS/Scenes/EventDetails/View/EventDetailsView.swift
@@ -49,6 +49,17 @@ class EventDetailsView: UIView {
         return lbl
     }()
     
+    public let scrollView: UIScrollView = {
+        let scrll = UIScrollView(frame: .zero)
+        scrll.translatesAutoresizingMaskIntoConstraints = false
+        return scrll
+    }()
+    
+    let containerScrollView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .systemBackground
@@ -61,29 +72,41 @@ class EventDetailsView: UIView {
     }
     
     private func addSubviews() {
-        self.addSubview(eventImageView)
-        self.addSubview(eventNameLabel)
-        self.addSubview(eventDateLabel)
-        self.addSubview(eventDescriptionLabel)
-        self.addSubview(eventPriceLabel)
+        self.addSubview(scrollView)
+        scrollView.addSubview(containerScrollView)
+        containerScrollView.addSubview(eventImageView)
+        containerScrollView.addSubview(eventNameLabel)
+        containerScrollView.addSubview(eventDateLabel)
+        containerScrollView.addSubview(eventDescriptionLabel)
+        containerScrollView.addSubview(eventPriceLabel)
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            eventImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            eventImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16),
+            scrollView.leftAnchor.constraint(equalTo: leftAnchor),
+            scrollView.rightAnchor.constraint(equalTo: rightAnchor),
+            scrollView.topAnchor.constraint(equalTo: topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            containerScrollView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            containerScrollView.leftAnchor.constraint(equalTo: scrollView.leftAnchor),
+            containerScrollView.rightAnchor.constraint(equalTo: scrollView.rightAnchor),
+            containerScrollView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            containerScrollView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            eventImageView.centerXAnchor.constraint(equalTo: containerScrollView.centerXAnchor),
+            eventImageView.topAnchor.constraint(equalTo: containerScrollView.topAnchor, constant: 16),
             eventImageView.widthAnchor.constraint(equalToConstant: 200),
             eventImageView.heightAnchor.constraint(equalToConstant: 200),
-            eventNameLabel.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor, constant: 16),
+            eventNameLabel.leftAnchor.constraint(equalTo: containerScrollView.leftAnchor, constant: 16),
             eventNameLabel.topAnchor.constraint(equalTo: eventImageView.bottomAnchor, constant: 8),
-            eventNameLabel.rightAnchor.constraint(equalTo: safeAreaLayoutGuide.rightAnchor, constant: -16),
+            eventNameLabel.rightAnchor.constraint(equalTo: containerScrollView.rightAnchor, constant: -16),
             eventDateLabel.leftAnchor.constraint(equalTo: eventNameLabel.leftAnchor),
             eventDateLabel.topAnchor.constraint(equalTo: eventNameLabel.bottomAnchor, constant: 16),
             eventPriceLabel.leftAnchor.constraint(equalTo: eventDateLabel.rightAnchor, constant: 8),
             eventPriceLabel.topAnchor.constraint(equalTo: eventDateLabel.topAnchor),
             eventDescriptionLabel.topAnchor.constraint(equalTo: eventDateLabel.bottomAnchor, constant: 8),
             eventDescriptionLabel.leftAnchor.constraint(equalTo: eventDateLabel.leftAnchor),
-            eventDescriptionLabel.rightAnchor.constraint(equalTo: safeAreaLayoutGuide.rightAnchor, constant: -16)
+            eventDescriptionLabel.rightAnchor.constraint(equalTo: containerScrollView.rightAnchor, constant: -16),
+            eventDescriptionLabel.bottomAnchor.constraint(equalTo: containerScrollView.bottomAnchor, constant: -16)
         ])
     }
 }
